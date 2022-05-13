a779710af3c4e059a740055e6dccf3519d3799d0
psyduck <1241280257@qq.com>
2022-05-13 15:52:36 +0800

    魔盒接口返回数据实体类

---
+++ ruoyi-robot/src/main/java/com/ruoyi/robot/entity/JX3BoxApiResult.java
@@ -0,0 +1,13 @@
+package com.ruoyi.robot.entity;
+/**
+ * 魔盒接口返回数据实体类
+ * @author psyduck
+ * @Date 2022/5/13 15:52
+ * @see
+ * @version 1.0
+ **/
+public class JX3BoxApiResult<T> {
+    private String msg;
+    private String code;
+    private T data;
+}
