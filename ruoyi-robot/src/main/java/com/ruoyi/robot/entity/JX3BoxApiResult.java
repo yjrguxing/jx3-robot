e7de03f91f25a0a09aa1eddfb5b83df56f1e2738
psyduck <1241280257@qq.com>
2022-05-14 15:27:38 +0800

    魔盒接口返回类尝试性优化（未启用

--- ruoyi-robot/src/main/java/com/ruoyi/robot/entity/JX3BoxApiResult.java
+++ ruoyi-robot/src/main/java/com/ruoyi/robot/entity/JX3BoxApiResult.java
@@ -1,4 +1,10 @@
 package com.ruoyi.robot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * 魔盒接口返回数据实体类
  * @author psyduck
@@ -6,6 +12,10 @@ package com.ruoyi.robot.entity;
  * @see
  * @version 1.0
  **/
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class JX3BoxApiResult<T> {
     private String msg;
     private String code;
